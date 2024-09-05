ccfcf0eb53de4c45b8a7f9763386036bb116b252
xavierchen <info@xavchen.com>
2024-09-05 14:32:40 +0100

    first save with HTML done

---
+++ app.js
@@ -0,0 +1,20 @@
+/**
+ * - The memory card board is initially displayed with all cards facing up.
+ * - Player XC begins the game by clicking on the card grid.
+ * - The corresponding timer is triggered when Player XC clicks on a card.
+ * - Player XC can select two cards at a time to check for matching patterns. If the patterns match, the cards remain face-up; if not, the cards flip back over with the front side up.
+ * - Player XC continues playing until all cards are matched and face-up.
+ * - After a brief delay following the completion of Player XC's game, the cards flip over and shuffle, and Player ML takes their turn.
+ *  - The winner is determined by the time taken to complete the game.
+ *  - Once Player ML finishes, A message appears to determine who the winner is with a "Start Again" button.
+ */
+
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
