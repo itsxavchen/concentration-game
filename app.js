711393a5bcdbe61d4f3e8b01190761e6de1fad8b
xavierchen <info@xavchen.com>
2024-09-06 16:17:22 +0100

    90% done

--- app.js
+++ app.js
@@ -13,43 +13,72 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let cardsBoard = ['', '', '', '', '', '', '', '', '', '', '', '']
 let firstClickOnCard = false
-let winner
-let firstCard
-let secondCard
+let firstCard = null
+let secondCard = null
+let lives = 5
 
 /*------------------------ Cached Element References ------------------------*/
 
 const cards = document.querySelectorAll('.card')
+const livesDisplay = document.querySelector('.lives')
+const buttonEl = document.querySelector('.button')
+
 /*-------------------------------- Functions --------------------------------*/
 
 function flipCard(event) {
     event.target.parentElement.classList.add('flip');
-
     if (firstClickOnCard === false) {
         firstCard = event.target.parentElement;
         firstClickOnCard = true;
     } else if (firstClickOnCard === true) {
         secondCard = event.target.parentElement;
         firstClickOnCard = false;
-
-        let firstCardImgSrc = firstCard.querySelector('.card-back').src;
-        let secondCardImgSrc = secondCard.querySelector('.card-back').src;
-
-        if (firstCardImgSrc === secondCardImgSrc) {
-            console.log('The images match!');
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-            console.log('The images do not match.');
-        }
+        matchCheck ()
     }
 }
 
+function loseLife() {
+    if (lives > 0) {
+        lives --
+        livesDisplay.innerHTML = lives
+    } else {
+        console.log('you are dead')
+        livesDisplay.innerHTML = 'DEAD'
+    }
+}
+
+function matchCheck () {
+    firstCardImgSrc = firstCard.querySelector('.card-back').src;
+    secondCardImgSrc = secondCard.querySelector('.card-back').src;
+    if (firstCardImgSrc === secondCardImgSrc) {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+    } else {
+        setTimeout(function () {
+            loseLife()
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+        }, 800)
+    }
+}
+
+function reset () {
+    lives = 5;
+    livesDisplay.innerHTML = lives;
+    firstCard = null
+    secondCard = null
+    firstClickOnCard = false
+    cards.forEach(function (card) {
+        card.classList.remove('flip');
+        card.addEventListener('click', flipCard);
+    })
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 cards.forEach(function (card) {
     card.addEventListener('click', flipCard)
 })
+
+buttonEl.addEventListener('click', reset)
